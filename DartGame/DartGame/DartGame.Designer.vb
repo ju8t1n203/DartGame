2d01d31aa421725e978567805a63a4bfc7686dc5
Ju8t1n203 <Juxin203@gmail.com>
2025-02-02 15:33:00 -0700

    Basic GUI

--- DartGame/DartGame/DartGame.Designer.vb
+++ DartGame/DartGame/DartGame.Designer.vb
@@ -25,11 +25,18 @@ Partial Class DartGame
         Me.NewTurnButton = New System.Windows.Forms.Button()
         Me.ViewSummaryButton = New System.Windows.Forms.Button()
         Me.ExitButton = New System.Windows.Forms.Button()
+        Me.StatsGroupBox = New System.Windows.Forms.GroupBox()
+        Me.TurnLabel = New System.Windows.Forms.Label()
+        Me.DartLabel = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.InstructionsLabel = New System.Windows.Forms.Label()
+        Me.StatsGroupBox.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'NewTurnButton
         '
-        Me.NewTurnButton.Location = New System.Drawing.Point(12, 384)
+        Me.NewTurnButton.Location = New System.Drawing.Point(210, 389)
         Me.NewTurnButton.Name = "NewTurnButton"
         Me.NewTurnButton.Size = New System.Drawing.Size(131, 54)
         Me.NewTurnButton.TabIndex = 0
@@ -38,7 +45,7 @@ Partial Class DartGame
         '
         'ViewSummaryButton
         '
-        Me.ViewSummaryButton.Location = New System.Drawing.Point(149, 384)
+        Me.ViewSummaryButton.Location = New System.Drawing.Point(347, 389)
         Me.ViewSummaryButton.Name = "ViewSummaryButton"
         Me.ViewSummaryButton.Size = New System.Drawing.Size(131, 54)
         Me.ViewSummaryButton.TabIndex = 0
@@ -47,28 +54,89 @@ Partial Class DartGame
         '
         'ExitButton
         '
-        Me.ExitButton.Location = New System.Drawing.Point(286, 384)
+        Me.ExitButton.Location = New System.Drawing.Point(484, 389)
         Me.ExitButton.Name = "ExitButton"
         Me.ExitButton.Size = New System.Drawing.Size(131, 54)
         Me.ExitButton.TabIndex = 0
         Me.ExitButton.Text = "E&xit"
         Me.ExitButton.UseVisualStyleBackColor = True
         '
+        'StatsGroupBox
+        '
+        Me.StatsGroupBox.Controls.Add(Me.DartLabel)
+        Me.StatsGroupBox.Controls.Add(Me.TurnLabel)
+        Me.StatsGroupBox.Location = New System.Drawing.Point(12, 387)
+        Me.StatsGroupBox.Name = "StatsGroupBox"
+        Me.StatsGroupBox.Size = New System.Drawing.Size(193, 51)
+        Me.StatsGroupBox.TabIndex = 1
+        Me.StatsGroupBox.TabStop = False
+        Me.StatsGroupBox.Text = "Stats"
+        '
+        'TurnLabel
+        '
+        Me.TurnLabel.AutoSize = True
+        Me.TurnLabel.Location = New System.Drawing.Point(7, 23)
+        Me.TurnLabel.Name = "TurnLabel"
+        Me.TurnLabel.Size = New System.Drawing.Size(32, 13)
+        Me.TurnLabel.TabIndex = 2
+        Me.TurnLabel.Text = "Turn:"
+        '
+        'DartLabel
+        '
+        Me.DartLabel.AutoSize = True
+        Me.DartLabel.Location = New System.Drawing.Point(97, 23)
+        Me.DartLabel.Name = "DartLabel"
+        Me.DartLabel.Size = New System.Drawing.Size(30, 13)
+        Me.DartLabel.TabIndex = 2
+        Me.DartLabel.Text = "Dart:"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.White
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 26)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(599, 344)
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'InstructionsLabel
+        '
+        Me.InstructionsLabel.AutoSize = True
+        Me.InstructionsLabel.Location = New System.Drawing.Point(244, 7)
+        Me.InstructionsLabel.Name = "InstructionsLabel"
+        Me.InstructionsLabel.Size = New System.Drawing.Size(132, 13)
+        Me.InstructionsLabel.TabIndex = 3
+        Me.InstructionsLabel.Text = "Press space to throw darts"
+        '
         'DartGame
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(623, 450)
+        Me.Controls.Add(Me.InstructionsLabel)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.StatsGroupBox)
         Me.Controls.Add(Me.ExitButton)
         Me.Controls.Add(Me.ViewSummaryButton)
         Me.Controls.Add(Me.NewTurnButton)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.Name = "DartGame"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Darts"
+        Me.StatsGroupBox.ResumeLayout(False)
+        Me.StatsGroupBox.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents NewTurnButton As Button
     Friend WithEvents ViewSummaryButton As Button
     Friend WithEvents ExitButton As Button
+    Friend WithEvents StatsGroupBox As GroupBox
+    Friend WithEvents TurnLabel As Label
+    Friend WithEvents DartLabel As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents InstructionsLabel As Label
 End Class
