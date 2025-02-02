e943cb37f0f9cc4602a9dc3e690e3b2e986ba3cf
Ju8t1n203 <Juxin203@gmail.com>
2025-02-02 15:10:49 -0700

    buttons added

--- DartGame/DartGame/DartGame.Designer.vb
+++ DartGame/DartGame/DartGame.Designer.vb
@@ -22,10 +22,53 @@ Partial Class DartGame
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.NewTurnButton = New System.Windows.Forms.Button()
+        Me.ViewSummaryButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'NewTurnButton
+        '
+        Me.NewTurnButton.Location = New System.Drawing.Point(12, 384)
+        Me.NewTurnButton.Name = "NewTurnButton"
+        Me.NewTurnButton.Size = New System.Drawing.Size(131, 54)
+        Me.NewTurnButton.TabIndex = 0
+        Me.NewTurnButton.Text = "&New Turn"
+        Me.NewTurnButton.UseVisualStyleBackColor = True
+        '
+        'ViewSummaryButton
+        '
+        Me.ViewSummaryButton.Location = New System.Drawing.Point(149, 384)
+        Me.ViewSummaryButton.Name = "ViewSummaryButton"
+        Me.ViewSummaryButton.Size = New System.Drawing.Size(131, 54)
+        Me.ViewSummaryButton.TabIndex = 0
+        Me.ViewSummaryButton.Text = "View &Summary"
+        Me.ViewSummaryButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(286, 384)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(131, 54)
+        Me.ExitButton.TabIndex = 0
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'DartGame
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ViewSummaryButton)
+        Me.Controls.Add(Me.NewTurnButton)
+        Me.Name = "DartGame"
+        Me.Text = "Darts"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents NewTurnButton As Button
+    Friend WithEvents ViewSummaryButton As Button
+    Friend WithEvents ExitButton As Button
 End Class
